fad2e618f35cb6450414e4a00480477924a4afa1
Arpit Kushwaha <arpit@example.com>
2025-06-15 11:50:48 +0530

    Fix: Update CORS to use FRONTEND_URI env variable

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URI || 'http://localhost:5173',
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true
