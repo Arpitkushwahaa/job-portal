6a4f55bbc5b1258ea0c5e36d3449b72977b4260d
Arpit Kushwaha <arpit@example.com>
2025-06-15 11:45:52 +0530

    Updated code with recent changes

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8000/api/v1";
+const API_BASE_URL = "https://job-portal-2-rrsg.onrender.com"
 export const USER_API_END_POINT = `${API_BASE_URL}/user`;
 export const JOB_API_END_POINT = `${API_BASE_URL}/job`;
 export const APPLICATION_API_END_POINT = `${API_BASE_URL}/application`;
